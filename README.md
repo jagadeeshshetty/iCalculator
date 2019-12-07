78603ebb696c610f0cb8acf40c181a19725fd997
JC <jagadeesh.cheluvaraj@gmail.com>
2019-12-07 20:36:44 +0530

    added multiply function and updated readme file.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 
 # Calculator API
 
+1.1.0
+- Add multiply support.
+
 1.0.0
 - Add two numbers.
--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 module.exports.add = (a, b) => {
     return a + b;
 }
+
+module.exports.multiply = (a, b) => {
+    return a * b;
+}
